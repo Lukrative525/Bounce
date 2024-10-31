3f23a9256cd24978a1f89f922322eb879509d503
Lukrative525 <lukegardner525@gmail.com>
2024-10-31 12:43:46 -0600

    add initializeGL and paintGL functions to draw a triangle on screen

--- graphicsviewer.cpp
+++ graphicsviewer.cpp
@@ -1,5 +1,18 @@
 #include "graphicsviewer.hpp"
 
+const char* vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "}\n\0";
+
 GraphicsViewer::GraphicsViewer(QWidget *parent):
     QOpenGLWidget{parent}
 {
@@ -8,5 +21,71 @@ GraphicsViewer::GraphicsViewer(QWidget *parent):
 
 void GraphicsViewer::initializeGL()
 {
-    qDebug() << "we\'re here!";
+    initializeOpenGLFunctions();
+
+    GLint success;
+    char infoLog[512];
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        qDebug() << "Vertex shader compilation failed:\n" << infoLog;
+    }
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        qDebug() << "Fragment shader compilation failed:\n" << infoLog;
+    }
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        qDebug() << "Shader program linking failed:\n" << infoLog;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] =
+    {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f
+    };
+
+    glGenVertexArrays(1, &vertexArrayObject);
+    glGenBuffers(1, &vertexBufferObject);
+
+    glBindVertexArray(vertexArrayObject);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+void GraphicsViewer::paintGL()
+{
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(shaderProgram);
+    glBindVertexArray(vertexArrayObject);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
--- graphicsviewer.hpp
+++ graphicsviewer.hpp
@@ -1,9 +1,10 @@
 #ifndef GRAPHICSVIEWER_HPP
 #define GRAPHICSVIEWER_HPP
 
+#include <QOpenGLExtraFunctions>
 #include <QOpenGLWidget>
 
-class GraphicsViewer : public QOpenGLWidget
+class GraphicsViewer: public QOpenGLWidget, protected QOpenGLExtraFunctions
 {
     Q_OBJECT
 public:
@@ -11,6 +12,10 @@ public:
 
 protected:
     void initializeGL() override;
+    void paintGL() override;
+    GLuint shaderProgram{0};
+    GLuint vertexBufferObject{0};
+    GLuint vertexArrayObject{0};
 };
 
 #endif // GRAPHICSVIEWER_HPP

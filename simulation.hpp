9c41d14a4168911f12265fca34652a986680e888
Lukrative525 <44730305+Lukrative525@users.noreply.github.com>
2024-10-31 18:28:24 -0600

    add simulation class

---
+++ simulation.hpp
@@ -0,0 +1,16 @@
+#ifndef SIMULATION_HPP
+#define SIMULATION_HPP
+
+class Simulation
+{
+public:
+    Simulation();
+};
+
+// reserve - reserves the memory for how ever many elements in vector
+// emplace_back - constructs ball in the vector
+
+// balls.reserve(max_balls);
+// balls.emplace_back({x, y, z});
+
+#endif // SIMULATION_HPP

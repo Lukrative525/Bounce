07edcd3dfdf1165e3de0e8f03db72fa6e1b10dd8
Lukrative525 <lukegardner525@gmail.com>
2024-12-09 21:15:23 -0700

    update physicsfunctions_unittests with test fixtures

--- physicsfunctions_unittests.cpp
+++ physicsfunctions_unittests.cpp
@@ -3,37 +3,39 @@
 #include "physicsfunctions.hpp"
 #include "vector3d.hpp"
 
-TEST(updateNextImplicitEuler, givenBallAtRestUnderAcceleration_WhenNextUpdated_ExpectAppropriateNextPositionAndVelocity)
+class UpdateNextImplicitEuler : public ::testing::Test
 {
+protected:
     Vector3D acceleration{1, 1, 1};
-    Ball ball{};
+    Ball ball{0};
     double timeStep{0.1};
 
-    phys::update_next_state_implicit_euler(timeStep, ball, acceleration);
+    void SetUp() override
+    {
+        phys::update_next_state_implicit_euler(timeStep, ball, acceleration);
+    }
+};
 
-    EXPECT_TRUE(ball.nextVelocity[0] == timeStep);
-    EXPECT_TRUE(ball.nextVelocity[1] == timeStep);
-    EXPECT_TRUE(ball.nextVelocity[2] == timeStep);
-    EXPECT_TRUE(ball.nextPosition[0] == timeStep * timeStep);
-    EXPECT_TRUE(ball.nextPosition[1] == timeStep * timeStep);
-    EXPECT_TRUE(ball.nextPosition[2] == timeStep * timeStep);
+TEST_F(UpdateNextImplicitEuler, givenBallAtRestUnderAcceleration_WhenNextStateUpdated_ExpectAppropriateNextPositionAndVelocity)
+{
+    EXPECT_TRUE(ball.nextVelocity.x == timeStep);
+    EXPECT_TRUE(ball.nextVelocity.y == timeStep);
+    EXPECT_TRUE(ball.nextVelocity.z == timeStep);
+    EXPECT_TRUE(ball.nextPosition.x == timeStep * timeStep);
+    EXPECT_TRUE(ball.nextPosition.y == timeStep * timeStep);
+    EXPECT_TRUE(ball.nextPosition.z == timeStep * timeStep);
 }
 
-TEST(ballConfirmNext, givenBallAtRestUnderAcceleration_WhenNextUpdatedAndConfirmed_ExpectAppropriatePositionAndVelocity)
+TEST_F(UpdateNextImplicitEuler, givenBallAtRestUnderAcceleration_WhenNextStateUpdatedAndConfirmed_ExpectAppropriatePositionAndVelocity)
 {
-    Vector3D acceleration{1, 1, 1};
-    Ball ball{};
-    double timeStep{0.1};
-
-    phys::update_next_state_implicit_euler(timeStep, ball, acceleration);
     ball.make_next_state_current();
 
-    EXPECT_TRUE(ball.velocity[0] == timeStep);
-    EXPECT_TRUE(ball.velocity[1] == timeStep);
-    EXPECT_TRUE(ball.velocity[2] == timeStep);
-    EXPECT_TRUE(ball.position[0] == timeStep * timeStep);
-    EXPECT_TRUE(ball.position[1] == timeStep * timeStep);
-    EXPECT_TRUE(ball.position[2] == timeStep * timeStep);
+    EXPECT_TRUE(ball.velocity.x == timeStep);
+    EXPECT_TRUE(ball.velocity.y == timeStep);
+    EXPECT_TRUE(ball.velocity.z == timeStep);
+    EXPECT_TRUE(ball.position.x == timeStep * timeStep);
+    EXPECT_TRUE(ball.position.y == timeStep * timeStep);
+    EXPECT_TRUE(ball.position.z == timeStep * timeStep);
 }
 
 TEST(reflectVector, givenAVectorAndNormal_WhenVectorFlipped_ExpectCorrectResult)
@@ -56,58 +58,64 @@ TEST(calculateDistanceBetween, givenTwoVectors_WhenDistanceBetweenCalculated_Exp
     EXPECT_EQ(distance, 5);
 }
 
-TEST(collisionsBetweenBallAndContainer, givenSuchCollision_WhenResolveFunctionCalled_ExpectCorrectFinalPosition)
+class DetectCollisionWithContainer : public ::testing::Test
 {
-    Ball movingBall;
-    movingBall.position[0] = 0;
-    movingBall.position[1] = 0;
-    movingBall.position[2] = 0;
-    movingBall.radius = 1;
-    movingBall.nextPosition[0] = 0;
-    movingBall.nextPosition[1] = 0;
-    movingBall.nextPosition[2] = 0;
-    Ball container;
-    container.position[0] = 0;
-    container.position[1] = 0;
-    container.position[2] = 0;
-    container.radius = 5;
-    container.nextPosition[0] = 0;
-    container.nextPosition[1] = 0;
-    container.nextPosition[2] = 0;
-    double distanceToImpact = container.radius - movingBall.radius;
+protected:
+    Ball movingBall{0};
+    Ball container{0};
 
-    movingBall.nextPosition[0] = distanceToImpact + 1;
+    void SetUp() override
+    {
+        movingBall.radius = 1;
+        container.radius = 2;
+    }
+};
 
+TEST_F(DetectCollisionWithContainer, givenNonOverlapping_WhenFunctionCalled_ExpectFalse)
+{
+    bool collisionDetected = phys::detect_collision_with_container(movingBall, container);
+
+    EXPECT_FALSE(collisionDetected);
+}
+
+TEST_F(DetectCollisionWithContainer, givenOverlapping_WhenFunctionCalled_ExpectTrue)
+{
+    movingBall.nextPosition.x = 1.75;
+
+    bool collisionDetected = phys::detect_collision_with_container(movingBall, container);
+
+    EXPECT_TRUE(collisionDetected);
+}
+
+class CollisionsBetweenBallAndContainer : public ::testing::Test
+{
+protected:
+    Ball movingBall{0};
+    Ball container{0};
+    double distanceToImpact;
+
+    void SetUp() override
+    {
+        movingBall.radius = 1;
+        container.radius = 5;
+
+        distanceToImpact = 4;
+
+        movingBall.nextPosition[0] = distanceToImpact + 1;
+    }
+};
+
+TEST_F(CollisionsBetweenBallAndContainer, givenSuchCollision_WhenResolveFunctionCalled_ExpectCorrectFinalPosition)
+{
     phys::resolve_collision_with_container(movingBall, container);
 
     EXPECT_TRUE(movingBall.nextPosition[0] == distanceToImpact);
 }
 
-TEST(collisionsBetweenBallAndContainer, givenSuchCollision_WhenResolveFunctionCalled_ExpectVelocityFlipped)
+TEST_F(CollisionsBetweenBallAndContainer, givenSuchCollision_WhenResolveFunctionCalled_ExpectVelocityFlipped)
 {
     double velocity{5};
-    Ball movingBall;
-    movingBall.position[0] = 0;
-    movingBall.position[1] = 0;
-    movingBall.position[2] = 0;
-    movingBall.radius = 1;
-    movingBall.elasticity = 1;
-    movingBall.nextPosition[0] = 0;
-    movingBall.nextPosition[1] = 0;
-    movingBall.nextPosition[2] = 0;
     movingBall.nextVelocity[0] = velocity;
-    Ball container;
-    container.position[0] = 0;
-    container.position[1] = 0;
-    container.position[2] = 0;
-    container.radius = 5;
-    container.elasticity = 1;
-    container.nextPosition[0] = 0;
-    container.nextPosition[1] = 0;
-    container.nextPosition[2] = 0;
-    double distanceToImpact = container.radius - movingBall.radius;
-
-    movingBall.nextPosition[0] = distanceToImpact + 1;
 
     phys::resolve_collision_with_container(movingBall, container);
 

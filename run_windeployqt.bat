9691d74c28ec575b7ea181a1460c2ef780ee0e00
Lukrative525 <lukegardner525@gmail.com>
2025-01-21 16:46:06 -0700

    update run_windeployqt.bat

--- run_windeployqt.bat
+++ run_windeployqt.bat
@@ -1,2 +1,2 @@
-cd c:\Users\Luke\Documents\CAE Assignments\final-project-Lukrative525\Bounce\build\Desktop_Qt_6_8_0_MSVC2022_64bit-Release
+cd build\Desktop_Qt_6_8_0_MSVC2022_64bit-Release
 c:\Qt\6.8.0\msvc2022_64\bin\windeployqt.exe Bounce.exe

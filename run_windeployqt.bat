f2c6531629de587f2bcbb018eb4ac71180601099
Lukrative525 <lukegardner525@gmail.com>
2025-03-26 14:25:30 -0600

    Update run_windeployqt.bat

--- run_windeployqt.bat
+++ run_windeployqt.bat
@@ -1,2 +1,21 @@
-cd build\Desktop_Qt_6_8_0_MSVC2022_64bit-Release
-c:\Qt\6.8.0\msvc2022_64\bin\windeployqt.exe Bounce.exe
+@echo off
+setlocal
+
+:: Define paths
+set QT_PATH=C:\Qt\6.8.3\msvc2022_64\bin
+set BUILD_DIR=c:\Users\Luke\Documents\software\Bounce\build\Desktop_Qt_6_8_3_MSVC2022_64bit-Release
+set TARGET_DIR=C:\Users\Luke\Documents\software\BounceRelease
+
+:: Create target directory if it doesn’t exist
+if not exist "%TARGET_DIR%" mkdir "%TARGET_DIR%"
+
+:: Copy the executable to the target directory
+echo Copying executable...
+copy "%BUILD_DIR%\Bounce.exe" "%TARGET_DIR%\Bounce.exe"
+
+:: Deploy the application
+echo Deploying application...
+"%QT_PATH%\windeployqt" --release --dir "%TARGET_DIR%" "%BUILD_DIR%\Bounce.exe"
+
+echo Deployment complete. Files are in: "%TARGET_DIR%"
+pause
